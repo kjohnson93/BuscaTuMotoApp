221a6804e0bbe07525bdfdbb21126f2196c0d0f1
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-01-21 09:37:45 +0100

    - Fixed android linkage error. Was not closing "@{viewModel.loadingVisibility}" statemente correctly. - Added toast message when we receiven an eror from API call

--- app/src/main/java/com/buscatumoto/ui/viewmodels/SearchFormViewModel.kt
+++ app/src/main/java/com/buscatumoto/ui/viewmodels/SearchFormViewModel.kt
@@ -3,12 +3,10 @@ package com.buscatumoto.ui.viewmodels
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.view.View
+import android.widget.Toast
+import com.buscatumoto.BuscaTuMotoApplication
 import com.buscatumoto.data.remote.configuration.BuscaTuMotoService
-import com.buscatumoto.injection.component.DaggerViewComponent
-import com.buscatumoto.injection.component.DaggerViewModelComponent
-import com.buscatumoto.injection.component.ViewComponent
-import com.buscatumoto.injection.component.ViewModelComponent
-import com.buscatumoto.injection.module.NetworkModule
+
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -51,6 +49,9 @@ class SearchFormViewModel @Inject constructor(): BaseViewModel() {
     }
 
     private fun onLoadFieldsError() {
+        val context = BuscaTuMotoApplication.getInstance().applicationContext
+
+        Toast.makeText(context, "Get Fields API error", Toast.LENGTH_LONG).show()
 
     }
 
