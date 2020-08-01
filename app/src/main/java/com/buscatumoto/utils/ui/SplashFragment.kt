62e8a6b31327d0762f6da1bbaef1fc1c9f7b69fe
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-08-01 19:12:30 +0200

    - Added fragment splash screen

---
+++ app/src/main/java/com/buscatumoto/utils/ui/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.buscatumoto.utils.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.buscatumoto.R
+
+class SplashFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_searchFragment)
+        }, 5000)
+
+    }
+}
