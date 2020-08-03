11764d3c15fa7a83b6e62b8742853250f8392d71
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-08-03 13:26:42 +0200

    - Added navigation drawer - Added toolbar - Added menu icon

--- app/src/main/java/com/buscatumoto/ui/activities/SearchActivity.kt
+++ app/src/main/java/com/buscatumoto/ui/activities/SearchActivity.kt
@@ -4,13 +4,16 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.Handler
+import android.view.MenuItem
 import android.view.View
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,6 +22,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.buscatumoto.R
 import com.buscatumoto.ui.fragments.SearchFragment
 import com.buscatumoto.utils.ui.CustomScrollView
+import com.google.android.material.navigation.NavigationView
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -32,6 +36,9 @@ class SearchActivity : AppCompatActivity(),
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     private lateinit var navController: NavController
+    private lateinit var mainToolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
 
 
@@ -41,6 +48,35 @@ class SearchActivity : AppCompatActivity(),
         setContentView(R.layout.activity_search)
 
         navController = findNavController(R.id.nav_host_fragment_search)
+        drawerLayout = findViewById<DrawerLayout>(R.id.searchDrawerLayout)
+        mainToolbar = findViewById<Toolbar>(R.id.mainToolbar)
+        navigationView = findViewById<NavigationView>(R.id.nav_view)
+
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.splashFragment, R.id.containerMainFragment, R.id.catalogueFragment,
+                R.id.motoDetailHostFragment), drawerLayout)
+        navigationView.setupWithNavController(navController)
+        mainToolbar.setupWithNavController(navController, appBarConfiguration)
+
+//        configureNavigationDrawer()
+//        configureToolbar()
+    }
+
+    private fun configureToolbar() {
+        setSupportActionBar(mainToolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_128)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun configureNavigationDrawer() {
+//        navigationView.setNavigationItemSelectedListener(
+//            object: NavigationView.OnNavigationItemSelectedListener {
+//                override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//
+//                }
+//
+//            })
     }
 
     override fun onReady() {
