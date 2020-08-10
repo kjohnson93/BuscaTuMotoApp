03dc0dc8116f03b33fca6135f181d0895139fd97
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-08-10 09:59:34 +0200

    - Refactored FilterRecyclerAdapter.kt and recyler item layout to clean up the code and show better code style

--- app/src/main/java/com/buscatumoto/ui/adapters/FilterRecyclerAdapter.kt
+++ app/src/main/java/com/buscatumoto/ui/adapters/FilterRecyclerAdapter.kt
@@ -15,44 +15,48 @@ import timber.log.Timber
 class FilterRecyclerAdapter(private val filterItemClickListener: FilterItemClickListener): RecyclerView.Adapter<FilterRecyclerAdapter.FilterRecyclerViewHolder>() {
 
     lateinit var filterItemsList : List<TestRecyclerItemData>
-    lateinit var viewModel: FilterViewModel
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilterRecyclerViewHolder {
         val binding: RecyclerBiketypeItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
             R.layout.recycler_biketype_item, parent, false)
-
-        return FilterRecyclerViewHolder(binding, parent.context)
+        return FilterRecyclerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: FilterRecyclerViewHolder, position: Int) {
-        val filterItem = filterItemsList[position]
-        holder.binding.itemCircleImg.setImageDrawable(filterItem.drawable)
-        holder.binding.itemCircleText.text = filterItem.title
-        if (filterItem.isSelected) {
-            holder.binding.itemCheckImg.visibility = View.VISIBLE
-        } else {
-            holder.binding.itemCheckImg.visibility = View.GONE
-        }
-        holder.binding.root.setOnClickListener {
-            filterItemClickListener.onClick(filterItem, position)
-        }
+        holder.bind(filterItemsList[position])
     }
 
     override fun getItemCount(): Int {
         return if (::filterItemsList.isInitialized) filterItemsList.size else 0
     }
 
-
     fun updateFilterItemsList(data: List<TestRecyclerItemData>) {
         this.filterItemsList = data
         notifyDataSetChanged()
     }
 
+    //ViewHolder are not meant to having a view model.
     inner class FilterRecyclerViewHolder(
-        val binding: RecyclerBiketypeItemBinding,
-        val context: Context):
-        RecyclerView.ViewHolder(binding.root) {
+        val binding: RecyclerBiketypeItemBinding):
+        RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
+        private lateinit var filterItem: TestRecyclerItemData
+
+        fun bind(filterItem: TestRecyclerItemData) {
+            this.filterItem = filterItem
+            binding.itemCircleImg.setImageDrawable(filterItem.drawable)
+            binding.itemCircleText.text = filterItem.title
+            binding.root.setOnClickListener(this)
+            if (filterItem.isSelected) {
+                binding.itemCheckImg.visibility = View.VISIBLE
+            } else {
+                binding.itemCheckImg.visibility = View.GONE
+            }
+        }
+
+        override fun onClick(v: View?) {
+            filterItemClickListener.onClick(filterItem, layoutPosition)
+        }
     }
 
     interface FilterItemClickListener {
