fae5a5d3591fa62239bfbc750e0e450f490c484d
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-09-12 18:58:52 +0200

    - Added google analytics for LanguagePickerFragment.kt

--- app/src/main/java/com/buscatumoto/ui/fragments/LanguagePickerFragment.kt
+++ app/src/main/java/com/buscatumoto/ui/fragments/LanguagePickerFragment.kt
@@ -14,10 +14,9 @@ import com.buscatumoto.databinding.FragmentLanguagePickerBinding
 import com.buscatumoto.injection.Injectable
 import com.buscatumoto.ui.activities.SearchActivity
 import com.buscatumoto.ui.viewmodels.LanguagePickerViewModel
-import com.buscatumoto.utils.global.LANGUAGE_CA
-import com.buscatumoto.utils.global.LANGUAGE_ENG
-import com.buscatumoto.utils.global.LANGUAGE_ES
+import com.buscatumoto.utils.global.*
 import com.buscatumoto.utils.injection.ViewModelFactory
+import com.google.firebase.analytics.FirebaseAnalytics
 import java.util.*
 import javax.inject.Inject
 
@@ -46,11 +45,35 @@ class LanguagePickerFragment: BaseFragment(), Injectable {
         binding.lifecycleOwner = this
 
         viewModel.languageSelectedMutable.observe(viewLifecycleOwner, Observer {
+            sendLanguageSelectedAnalytics(it)
             val activity = requireActivity() as SearchActivity
             activity.setLanguage(it)
         })
 
         return binding.root
+    }
+
+    private fun sendLanguageSelectedAnalytics(lang: String) {
+        when (lang) {
+            LANGUAGE_ES -> {
+                val bundle = Bundle()
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, LANGUAGE_ES_ITEM_ID)
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, LANG_CONTENT_TYPE)
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+            }
+            LANGUAGE_ENG -> {
+                val bundle = Bundle()
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, LANGUAGE_EN_ITEM_ID)
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, LANG_CONTENT_TYPE)
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+            }
+            LANGUAGE_CA -> {
+                val bundle = Bundle()
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, LANGUAGE_CA_ITEM_ID)
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, LANG_CONTENT_TYPE)
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+            }
+        }
     }
 
     override fun onDestroyView() {
