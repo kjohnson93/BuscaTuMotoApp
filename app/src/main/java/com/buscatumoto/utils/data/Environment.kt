0f1587a2c95d1775c5928202d7090667786b2f16
Kevin Johnson <k.johnson.kvk@gmail.com>
2020-10-03 17:07:04 +0200

    Added misc changes

--- app/src/main/java/com/buscatumoto/utils/data/Environment.kt
+++ app/src/main/java/com/buscatumoto/utils/data/Environment.kt
@@ -5,10 +5,6 @@ package com.buscatumoto.utils.data
  * API URL environment.
  */
 enum class Environment  constructor(val title: String,val path: String,val id: Int) {
-//    DEVELOP("DEV", "http://10.10.1.126:8080/", 1),
-    //    DEVELOP("DEV", "http://192.168.1.18:8080/", 1),
-    //10.10.1.126
-    //192.168.1.12
-    DEVELOP("DEV", "http://192.168.1.12:8080/", 1),
+    DEVELOP("DEV", "http://192.168.1.12:8080/", 1), //Replace by domain url
     RELEASE("RELEASE", "http://192.168.1.12:8080/", 2)
 }
